b97339b4bf89376c8435e64587a070a54ecfb381
SpencerX950 <41224404+SpencerX950@users.noreply.github.com>
2018-07-16 17:11:06 +0800

    Update UserAuthentication.java

--- app/src/main/java/com/chookstogo/orderingsystem/UserAuthentication.java
+++ app/src/main/java/com/chookstogo/orderingsystem/UserAuthentication.java
@@ -1,5 +1,41 @@
-// Used for Registration and Login User
-public UserAuthentication 
-{
+package com.chookstogo.orderingsystem;
 
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class UserAuthentication{
+    private User user;
+    protected DatabaseReference fireauth;
+
+
+    public void setNewUser(User user)
+    {
+        //Database Init
+        //fireauth = FirebaseDatabase.getInstance().getReference();
+        fireauth.push().setValue(user.getUserMap()).addOnCompleteListener(new OnCompleteListener<Void>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<Void> task)
+            {
+                if(task.isSuccessful()){
+                    //do something
+                }
+                else
+                {
+                    //do something
+                }
+            }
+        });
+    }
+
+
+    public User getNewUser() {
+        return user;
+    }
 }
